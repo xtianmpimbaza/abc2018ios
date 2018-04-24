26d4996fc6671734f50b1da993cd3d9bf23a2554
christian <xtianm4@gmail.com>
2018-04-24 12:27:54 +0300

    emailcomposer gmail added

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -4,6 +4,7 @@ import {IonicPage, LoadingController, NavController, NavParams, ViewController} 
 import {Validators, FormBuilder, FormGroup} from "@angular/forms";
 import {GlobalVars} from "../../providers/global-vars";
 import {Http} from "@angular/http";
+import {EmailComposer} from "@ionic-native/email-composer";
 
 @IonicPage()
 @Component({
@@ -20,14 +21,15 @@ export class ModalPage {
     public navParams: NavParams,
     public viewCtrl: ViewController,
     private global: GlobalVars,
+    private emailComposer: EmailComposer,
     private http: Http,
     private loadingCtrl: LoadingController,
               private formBuilder: FormBuilder) {
     this.receipient = this.navParams.get('mail_receipiennt');
     this.credentialsForm = this.formBuilder.group({
       email_to: this.receipient,
-      email_from: ['',Validators.compose([Validators.pattern(
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/), Validators.required])],
+      // email_from: ['',Validators.compose([Validators.pattern(
+      //   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/), Validators.required])],
       no_html: ['']
     });
     this.data.response = '';
@@ -45,31 +47,60 @@ export class ModalPage {
   public send() {
     // console.log(this.credentialsForm.value);
 
-    let loader = this.loadingCtrl.create({
-      content: "Sending....."
+    this.emailComposer.isAvailable().then((available: boolean) =>{
+      if(available) {
+        //Now we know we can send
+        console.log("mail available");
+      }
     });
-    loader.present();
 
-    new Promise(resolve => {
-      this.http.post(this.global.api_url, this.credentialsForm.value)
-        .map(res => res.json())
-        .subscribe((result) => {
-            console.log(result);
+    let email = {
+      app: 'gmail',
+      to: this.credentialsForm.value.email_to,
+      cc: 'xtianmpimbaza@gmail.com',
+      bcc: ['christianm@autosoftug.com', 'christianm@autosoftug.com'],
+      subject: 'Interaction with african blockchain conference attendee',
+      body: this.credentialsForm.value.no_html,
+      isHtml: true
+    };
 
-            if (result.feedback =='success') {
-              this.global.toast("Email sent.", 'toast-error');
-              loader.dismiss();
-              this.closeModal();
-            } else {
-                this.global.toast("Error occurred, Email not sent", 'toast-error');
-                loader.dismiss();
-            }
-          },
-          (err) => {
-            this.global.toast("Error occurred, Check your internet connection", 'toast-error');
-            loader.dismiss();
-          });
-    });
+    // this.emailComposer.addAlias('gmail', 'com.google.android.gm');
+//
+// // then use alias when sending email
+//     this.emailComposer.open({
+//       app: 'gmail',
+//       ...
+//     });
+
+// Send a text message using default options
+    this.emailComposer.open(email);
+
+
+    // let loader = this.loadingCtrl.create({
+    //   content: "Sending....."
+    // });
+    // loader.present();
+    //
+    // new Promise(resolve => {
+    //   this.http.post(this.global.api_url, this.credentialsForm.value)
+    //     .map(res => res.json())
+    //     .subscribe((result) => {
+    //         console.log(result);
+    //
+    //         if (result.feedback =='success') {
+    //           this.global.toast("Email sent.", 'toast-error');
+    //           loader.dismiss();
+    //           this.closeModal();
+    //         } else {
+    //             this.global.toast("Error occurred, Email not sent", 'toast-error');
+    //             loader.dismiss();
+    //         }
+    //       },
+    //       (err) => {
+    //         this.global.toast("Error occurred, Check your internet connection", 'toast-error');
+    //         loader.dismiss();
+    //       });
+    // });
 
   }
 
